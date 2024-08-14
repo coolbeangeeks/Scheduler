af48d4c374dac2554426554f7f11f1ce09c25554
coolbeangeeks <abhigupta96677@gmail.com>
2024-08-14 16:51:42 +0530

    dummay data

---
+++ src/utils/constant.js
@@ -0,0 +1,31 @@
+{
+    "students": [
+      {
+        "id": 1,
+        "name": "Aakash",
+        "areaOfInterest": "Digital Marketing",
+        "availability": ["Sunday", "Monday", "Wednesday"]
+      },
+      {
+        "id": 2,
+        "name": "Abhinav",
+        "areaOfInterest": "FMCG Sales",
+        "availability": ["Tuesday", "Thursday", "Saturday"]
+      }
+    ],
+    "mentors": [
+      {
+        "id": 1,
+        "name": "Sandeep",
+        "areaOfExpertise": ["Digital Marketing"],
+        "availability": ["Sunday", "Saturday"]
+      },
+      {
+        "id": 2,
+        "name": "Vijay",
+        "areaOfExpertise": ["FMCG Sales"],
+        "availability": ["Monday", "Wednesday", "Friday"]
+      }
+    ]
+}
+  
